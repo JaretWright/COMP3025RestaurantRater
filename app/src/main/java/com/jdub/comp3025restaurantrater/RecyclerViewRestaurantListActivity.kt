9ed4be07eb88a8a9c648a730489d0d9645609e11
JaretWright <jaret.wright@georgiancollege.ca>
2021-03-02 14:56:18 -0500

    Connected the RecyclerView with the Activity

--- app/src/main/java/com/jdub/comp3025restaurantrater/RecyclerViewRestaurantListActivity.kt
+++ app/src/main/java/com/jdub/comp3025restaurantrater/RecyclerViewRestaurantListActivity.kt
@@ -2,10 +2,23 @@ package com.jdub.comp3025restaurantrater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.jdub.comp3025restaurantrater.databinding.ActivityRecyclerViewRestaurantListBinding
 
 class RecyclerViewRestaurantListActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityRecyclerViewRestaurantListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recycler_view_restaurant_list)
+        binding = ActivityRecyclerViewRestaurantListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //get the data from the viewmodel
+        val model : RestaurantListViewModel by viewModels()
+        model.getRestaurants().observe(this, Observer<List<Restaurant>>{ restaurants->
+            var recyclerAdapter = RecyclerViewAdapter(this, restaurants)
+            binding.verticalRecyclerView.adapter = recyclerAdapter
+        })
     }
 }
