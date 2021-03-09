706aeb550bd847f7668a584672c5ecf735b84456
JaretWright <jaret.wright@georgiancollege.ca>
2021-03-09 13:13:11 -0500

    Connected the RecyclerView with the Activity

---
+++ app/src/main/java/com/jdub/comp3025restaurantrater/CommentActivity.kt
@@ -0,0 +1,11 @@
+package com.jdub.comp3025restaurantrater
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment)
+    }
+}
---
+++ app/src/main/java/com/jdub/comp3025restaurantrater/RecyclerViewGridActivity.kt
@@ -0,0 +1,25 @@
+package com.jdub.comp3025restaurantrater
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.jdub.comp3025restaurantrater.databinding.ActivityRecyclerViewGridBinding
+
+
+class RecyclerViewGridActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityRecyclerViewGridBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRecyclerViewGridBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //get the data from the viewmodel
+        val model : RestaurantListViewModel by viewModels()
+        model.getRestaurants().observe(this, Observer<List<Restaurant>>{ restaurants->
+            var recyclerAdapter = RecyclerGridAdapter(this, restaurants)
+            binding.gridRecyclerView.adapter = recyclerAdapter
+        })
+    }
+}
