d9be9b7b48641401c19119a04e3c39b20845322b
JaretWright <jaret.wright@georgiancollege.ca>
2021-03-09 13:40:20 -0500

    Added the Comment.kt file

---
+++ app/src/main/java/com/jdub/comp3025restaurantrater/Comment.kt
@@ -0,0 +1,8 @@
+package com.jdub.comp3025restaurantrater
+
+data class Comment (
+    var commentID: String?=null,
+    var userName: String?=null,
+    var body: String?=null,
+    var restaurantID: String?=null
+        )
--- app/src/main/java/com/jdub/comp3025restaurantrater/CommentActivity.kt
+++ app/src/main/java/com/jdub/comp3025restaurantrater/CommentActivity.kt
@@ -2,10 +2,18 @@ package com.jdub.comp3025restaurantrater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jdub.comp3025restaurantrater.databinding.ActivityCommentBinding
 
 class CommentActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityCommentBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_comment)
+        binding = ActivityCommentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.restaurantNameTextView.text = intent.getStringExtra("restaurantName")
+        val restaurantID = intent.getStringExtra("restaurantID")
+
     }
 }
